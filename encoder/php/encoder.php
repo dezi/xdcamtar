4be93c51fb5e30cd912435a0354389f645dc89b9
Kappa <kappa@odroid-u2>
2015-03-06 10:54:00 +0100

    Check in.

--- encoder/php/encoder.php
+++ encoder/php/encoder.php
@@ -2,6 +2,7 @@
 
 $GLOBALS[ "encoder"  ] = "1.0.0.1004";
 
+$GLOBALS[ "servers"  ][] = "192.168.50.1:8880";
 $GLOBALS[ "servers"  ][] = "PC15930.spiegel.de:8880";
 $GLOBALS[ "servers"  ][] = "192.168.55.244:8880";
 $GLOBALS[ "servers"  ][] = "dezimac.local:80";
